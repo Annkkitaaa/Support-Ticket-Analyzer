7def70f9f9f2fe3e098f46debb3a0b6f05395639
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-01-05 22:20:15 +0530

    added sample tickets

---
+++ sample_tickets.py
@@ -0,0 +1,21 @@
+SAMPLE_TICKETS = [
+    "I can't log into my account. I've tried resetting my password 3 times but still getting an error.",
+    
+    "The export feature is not working. I need to download my data urgently for a presentation tomorrow morning.",
+    
+    "I would love to see a dark mode option in the next update. The current interface is too bright.",
+    
+    "My last invoice shows incorrect charges. I was billed twice for the same service.",
+    
+    "The mobile app keeps crashing whenever I try to upload images.",
+    
+    "Can you add integration with Google Calendar? It would make scheduling much easier.",
+    
+    "The system is extremely slow today. Pages are taking 30+ seconds to load.",
+    
+    "How do I change my subscription plan? I want to upgrade to premium.",
+    
+    "The search function isn't working properly. It's not showing any results.",
+    
+    "Need help setting up automated reports. The documentation isn't clear."
+]
